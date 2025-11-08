1a7c052882623b3924e447945b798a85b00701ef
Anderson Bernardo <bernardoanderson104@gmail.com>
2025-11-08 00:38:01 -0300

    Conclusão da atividade Budega

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -10,14 +10,51 @@ class Person:
     def __str__(self):
         return f"nome: {self.__nome}"
 
-
 class Market:
     def __init__(self):
         self.__counters: list[Person | None] = []
-        self.__queue: list[Person]
+        self.__queue: list[Person] = []
+
+    def init(self, qtd_counters: int = 0):
+        for _ in range(qtd_counters):
+            self.__counters.append(None)
+
+    def arrive(self, person: Person):
+        self.__queue.append(person)
+
+    def call(self, index: int):
+        if index < 0 or index >= len(self.__counters):
+            print("fail: caixa nao existe")
+            return
+        if self.__counters[index] is not None:
+            print("fail: caixa ocupado")
+            return
+        if len(self.__queue) == 0:
+            print("fail: sem clientes")
+            return
+        self.__counters[index] = self.__queue[0]
+        del self.__queue[0]
+
+    def finish(self, index: int):
+        if index < 0 or index >= len(self.__counters):
+            print("fail: caixa inexistente")
+            return
+        if self.__counters[index] is None:
+            print("fail: caixa vazio")
+            return
+        self.__counters[index] = None
+
+    def give_up(self, name: str) -> Person | None:
+        for i, person in enumerate(self.__queue):
+            if person.name == name:
+                aux = self.__queue[i]
+                del self.__queue[i]
+                return aux
 
     def __str__(self):
-        return f"Caixas: {self.__counters} // Espera: {self.__queue}"
+        counters = ", ".join(["-----" if x is None else str(x) for x in self.__counters])
+        queue = ", ".join([str(x) for x in self.__queue])
+        return f"Caixas: [{counters}]\nEspera: [{queue}]"
 
 def main():
     budega = Market()
@@ -29,5 +66,20 @@ def main():
             break
         elif args[0] == "show":
             print(budega)
+        elif args[0] == "init":
+            qtd_counters = int(args[1])
+            budega.init(qtd_counters)
+        elif args[0] == "arrive":
+            person = args[1]
+            budega.arrive(person)
+        elif args[0] == "call":
+            index = int(args[1])
+            budega.call(index)
+        elif args[0] == "finish":
+            index = int(args[1])
+            budega.finish(index)
+        elif args[0] == "giveup":
+            name = args[1]
+            budega.give_up(name)
 
 main()
